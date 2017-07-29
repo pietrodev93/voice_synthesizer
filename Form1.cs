d0a9d4f2cfaad412b2e1c0bbdbcce2e089b8165a
pietrodev93 <pietrogiordano93@gmail.com>
2017-07-29 12:47:00 +0200

    Only file form.cs

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace Sintetizzatore_vocale
+{
+    public partial class frmainForm : Form
+    {
+        SpeechSynthesizer sintetizzatore;
+        public frmainForm()
+        {
+            InitializeComponent();
+            sintetizzatore = new SpeechSynthesizer();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btleggiTesto_Click(object sender, EventArgs e)
+        {
+            sintetizzatore.SpeakAsync(tbmessaggioUtente.Text);
+        }
+
+        private void frmainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            sintetizzatore.SpeakAsyncCancelAll();
+            sintetizzatore.Dispose();
+        }
+    }
+}
